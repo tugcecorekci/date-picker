e53d092c899c1da2617495ef4d5cd149c69e3f72
tugcecorekci <tugcecorekcioglu@gmail.com>
2022-03-02 11:29:56 +0100

    Updated date selection in previous and next month functions.

--- app.js
+++ app.js
@@ -18,14 +18,14 @@ let currentMinute = String(today.getMinutes()).padStart(2, '0')
 
 //screening current date
 
-function addCurrentDate() {
+function addCurrentDateInput() {
     let stringDate = today.toLocaleDateString()
     screenInput.value = `${stringDate} ${currentHour}:${currentMinute}`
     let monthName = months[currentMonth]
     selectedMonthYear.textContent = `${monthName} ${currentYear}`
 }
 
-addCurrentDate()
+addCurrentDateInput()
 
 //adding current time to screenInput
 
@@ -60,6 +60,7 @@ function daysOfMonth() {
             let prevDay = document.createElement('div')
             prevDay.textContent = new Date(currentYear, currentMonth, 0).getDate() - j + 1
             prevDay.setAttribute('id', 'previous')
+            prevDay.addEventListener('click', () => makeCurrent(prevDay))
             tableDays.appendChild(prevDay)
             prevDayCount += 1
         }
@@ -67,10 +68,12 @@ function daysOfMonth() {
     for (let i = 0; i < dayCount; i++) {
         let dayNum = document.createElement('div')
         dayNum.textContent = i + 1
+        dayNum.setAttribute('id', 'uptodate')
         tableDays.appendChild(dayNum)
-        if (i + 1 == currentDate) {
-            dayNum.setAttribute('class', 'current')
-        }
+        dayNum.addEventListener('click', () => makeCurrent(dayNum))
+        // if (i + 1 == currentDate) {
+        //     dayNum.setAttribute('class', 'current')
+        // }
     }
     let daysCount = prevDayCount + lastDay
     let nextMonthDay = null
@@ -79,16 +82,42 @@ function daysOfMonth() {
         nextMonthDay += 1
         nextDay.textContent = nextMonthDay
         nextDay.setAttribute('id', 'next')
+        nextDay.addEventListener('click', () => makeCurrent(nextDay))
         tableDays.appendChild(nextDay)
         daysCount += 1
     }
 }
 
+function makeCurrent(e) {
+    let selectCurrent = document.querySelector('.current')
+    selectCurrent.removeAttribute('class')
+    e.setAttribute('class', 'current')
+    console.log(e.textContent)
+    // if (e.className == 'previous') {
+    //     previousMonth()
+    // }
+    // else if (e.className == 'next') {
+    //     nextMonth()
+    // }
+}
+
 daysOfMonth()
+
+//mark current day - set class = current
+function getCurrent() {
+    let cd = document.querySelectorAll('#uptodate')
+    cd.forEach(c => {
+        if (c.textContent == currentDate) {
+            c.setAttribute('class', 'current')
+        }
+    })
+}
+getCurrent()
 
 //prev-next
 previousBtn.addEventListener('click', previousMonth)
 function previousMonth() {
+    let lastCurrent = document.querySelector('.current').textContent
     while (tableDays.firstChild) {
         tableDays.removeChild(tableDays.lastChild);
     }
@@ -99,12 +128,21 @@ function previousMonth() {
     }
     today.setMonth(currentMonth)
     daysOfMonth()
-    addCurrentDate()
+    let newCurrent = document.querySelectorAll('#uptodate')
+    newCurrent.forEach(day => {
+        if (day.textContent == lastCurrent) {
+            day.setAttribute('class', 'current')
+        }
+    })
+    addCurrentDateInput()
     changeTime()
 }
 
+
 nextBtn.addEventListener('click', nextMonth)
 function nextMonth() {
+    let lastCurrent = document.querySelector('.current').textContent
+
     while (tableDays.firstChild) {
         tableDays.removeChild(tableDays.lastChild);
     }
@@ -115,6 +153,23 @@ function nextMonth() {
     }
     today.setMonth(currentMonth)
     daysOfMonth()
-    addCurrentDate()
+
+    let newCurrent = document.querySelectorAll('#uptodate')
+    let newDayCount = new Date(currentYear, currentMonth + 1, 0).getDate()
+    if (lastCurrent > newDayCount) {
+        for (let x = 0; x < newDayCount; x++) {
+            if (newCurrent[x].textContent == newDayCount) {
+                newCurrent[x].setAttribute('class', 'current')
+            }
+
+        }
+    }
+    newCurrent.forEach(day => {
+        if (day.textContent == lastCurrent) {
+            day.setAttribute('class', 'current')
+        }
+    })
+
+    addCurrentDateInput()
     changeTime()
 }
